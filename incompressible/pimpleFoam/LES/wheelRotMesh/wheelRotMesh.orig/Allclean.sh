b2a53474f7de3431051559c55736331396379526
Henrik Rusche <h.rusche@wikki-gmbh.de>
2023-04-05 16:41:43 +0200

    Adding wheelRotMesh case (1/4)

---
+++ incompressible/pimpleFoam/LES/wheelRotMesh/wheelRotMesh.orig/Allclean.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -l
+rm -r processor* postProcessing logFiles 0
+rm -r constant/extendedFeatureEdgeMesh constant/polyMesh constant/dynamicMeshDict
+# ------------------------------------------------------------ end-of-file                                                                
